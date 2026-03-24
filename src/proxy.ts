b4e185ad5b71e727b26c03e2c2be88016af2e8e7
Hendrius Dass <hendriusfelix.dev@gmail.com>
2026-03-24 08:31:28 -0300

    fix(telas): corrige url de redirecionamento

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,9 +2,9 @@ import { vars } from "./config/dotenv";
 import { createProxyMiddleware } from "http-proxy-middleware"
 
 export const setupProxy = (app: any, server: any) => {
-  app.use("/api/telas/",
+  app.use("/api/telas",
     createProxyMiddleware({
-      target: vars.MAIN_SERVICE,
+      target: vars.TELAS_SERVICE,
       changeOrigin: true,
       pathRewrite: { "^/api/telas": "" },
     })
